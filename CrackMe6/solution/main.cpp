8fa3941c107dfdc9bcefc16c396dbd13557cb92d
CapitanSlider <d-a-n-max@yandex.ru>
2024-03-04 22:26:59 +0500

    Add CrackMe6

---
+++ CrackMe6/solution/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <random>
+#include <string>
+#include <sstream>
+int main() {
+	std::random_device dev;
+	std::mt19937 rng(dev());
+	std::uniform_int_distribution<std::mt19937::result_type> dist(0, 9);
+	std::stringstream ss;
+	ss << "A";
+	for (int i = 0; i < 10; ++i) {
+		if (i == 2 || i == 6) {
+			ss << "X";
+			continue;
+		}
+		ss << dist(rng);
+	}
+	std::cout << ss.str();
+}
